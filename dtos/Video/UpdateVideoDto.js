bbec702842a86f3ed761662b4de630b8577a33c8
Nguyen Khanh Tung <ktung7574@gmail.com>
2024-10-17 15:44:04 +0700

    document video for swagger

--- dtos/Video/UpdateVideoDto.js
+++ dtos/Video/UpdateVideoDto.js
@@ -2,6 +2,41 @@ const StatusCodeEnums = require("../../enums/StatusCodeEnum");
 const CoreException = require("../../exceptions/CoreException");
 const { validMongooseObjectId } = require("../../utils/validator");
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     UpdateVideoDto:
+ *       type: object
+ *       required:
+ *         - title
+ *         - description
+ *         - categoryIds
+ *         - enumMode
+ *         - thumbnailUrl
+ *       properties:
+ *         title:
+ *           type: string
+ *           default: example
+ *           description: Video's title
+ *         description:
+ *           type: string
+ *           default: example description
+ *           description: Video's description
+ *         categoryIds:
+ *           type: array
+ *           default: []
+ *           description: A list of category IDs
+ *         enumMode:
+ *           type: string
+ *           default: public
+ *           description: Value of enum mode [public, private, unlisted]
+ *         thumbnailUrl:
+ *           type: string
+ *           default: https://example.com
+ *           description: URL of thumbnail
+ */
+
 class UpdateVideoDto {
   constructor(videoId, videoThumbnailFile) {
     this.videoId = videoId;
--- routes/VideoRoute.js
+++ routes/VideoRoute.js
@@ -6,11 +6,28 @@ const { uploadVideo, uploadImage } = require("../utils/stores/storeImage");
 const videoRoutes = express.Router();
 const videoController = new VideoController();
 
-videoRoutes.post(
-  "/",
-  AuthMiddleware,
-  videoController.createVideoController
-);
+/**
+ * @swagger
+ * /api/videos/:
+ *  post:
+ *    tags: [Video]
+ *    summary: Create a video
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/CreateVideoDto'
+ *    responses:
+ *      201:
+ *        description: Create video successfully
+ *      400:
+ *        description: Bad request
+ *      500:
+ *        description: Internal server error
+ */
+
+videoRoutes.post("/", AuthMiddleware, videoController.createVideoController);
 
 videoRoutes.put(
   "/:videoId",
@@ -19,15 +36,88 @@ videoRoutes.put(
   videoController.uploadVideoController
 );
 
+/**
+ * @swagger
+ * /api/videos/:
+ *  get:
+ *   summary: Get all videos
+ *   tags: [Video]
+ *   responses:
+ *    200:
+ *      description: Get all videos successfully
+ *    400:
+ *      description: Bad request
+ *    500:
+ *      description: Internal server error
+ */
 videoRoutes.get("/", AuthMiddleware, videoController.getVideosController);
 
+/**
+ * @swagger
+ * /api/videos/user/{userId}:
+ *  get:
+ *    summary: Get videos by user id
+ *    tags: [Video]
+ *    parameters:
+ *      - in: path
+ *        name: user Id
+ *        required: true
+ *        schema:
+ *          type: string
+ *          description: User ID
+ *    200:
+ *      description: Get videos by user id successfully
+ *    400:
+ *      description: Bad request
+ *    500:
+ *      description: Internal server error
+ */
 videoRoutes.get("/user/:userId", videoController.getVideosByUserIdController);
 
+/**
+ * @swagger
+ * /api/videos/my-playlist/{playlistId}:
+ *  get:
+ *    summary: Get videos by playlist id
+ *    tags: [Video]
+ *    parameters:
+ *      - in: path
+ *        name: playlist Id
+ *        required: true
+ *        schema:
+ *          type: string
+ *          description: Playlist ID
+ *    200:
+ *      description: Get videos by playlist id successfully
+ *    400:
+ *      description: Bad request
+ *    500:
+ *      description: Internal server error
+ */
 videoRoutes.get(
   "/my-playlist/:playlistId",
   videoController.getVideosByPlaylistIdController
 );
 
+/**
+ * @swagger
+ * /api/videos/{videoId}:
+ *  patch:
+ *    summary: Update a video by id
+ *    tags: [Video]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/UpdateVideoDto'
+ *    200:
+ *      description: Get videos by playlist id successfully
+ *    400:
+ *      description: Bad request
+ *    500:
+ *      description: Internal server error
+ */
 videoRoutes.patch(
   "/:videoId",
   AuthMiddleware,
@@ -35,11 +125,67 @@ videoRoutes.patch(
   videoController.updateAVideoByIdController
 );
 
+/**
+ * @swagger
+ * /api/videos/{videoId}:
+ *  get:
+ *   summary: Get video by id
+ *   tags: [Video]
+ *   parameters:
+ *      - in: path
+ *        name: video Id
+ *        required: true
+ *        schema:
+ *          type: string
+ *          description: Video ID
+ *   responses:
+ *    200:
+ *      description: Get video by id successfully
+ *    400:
+ *      description: Bad request
+ *    500:
+ *      description: Internal server error
+ */
+
 videoRoutes.get(
   "/:videoId",
   AuthMiddleware,
   videoController.getVideoController
 );
+
+/**
+ * @swagger
+ * /api/videos/like/{videoId}:
+ *  post:
+ *   summary: Toggle like a video
+ *   tags: [Video]
+ *   parameters:
+ *      - in: path
+ *        name: videoId
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: Video ID
+ *      - in: query
+ *        name: action
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: Action
+ *      - in: header
+ *        name: userId
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: User ID
+ *   responses:
+ *    200:
+ *      description: Toggle like video by id successfully
+ *    400:
+ *      description: Bad request
+ *    500:
+ *      description: Internal server error
+ */
 
 videoRoutes.post(
   "/like/:videoId",
@@ -47,11 +193,55 @@ videoRoutes.post(
   videoController.toggleLikeVideoController
 );
 
+/**
+ * @swagger
+ * /api/videos/view/{videoId}:
+ *  post:
+ *   summary: Increase view of a video
+ *   tags: [Video]
+ *   parameters:
+ *      - in: path
+ *        name: videoId
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: Video ID
+ *   responses:
+ *    200:
+ *      description: Increase view of a video by id successfully
+ *    400:
+ *      description: Bad request
+ *    500:
+ *      description: Internal server error
+ */
+
 videoRoutes.post(
   "/view/:videoId",
   AuthMiddleware,
   videoController.viewIncrementController
 );
+
+/**
+ * @swagger
+ * /api/videos/{videoId}:
+ *  delete:
+ *   summary: Delete a video
+ *   tags: [Video]
+ *   parameters:
+ *      - in: path
+ *        name: videoId
+ *        required: true
+ *        schema:
+ *          type: string
+ *        description: Video ID
+ *   responses:
+ *    200:
+ *      description: Delete video by id successfully
+ *    400:
+ *      description: Bad request
+ *    500:
+ *      description: Internal server error
+ */
 
 videoRoutes.delete(
   "/:videoId",
