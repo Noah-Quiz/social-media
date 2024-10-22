a55bf793de25b6dac8bd37dee01097025b7652d4
Nguyen Khanh Tung <ktung7574@gmail.com>
2024-10-22 16:17:13 +0700

    update advertisement

--- repositories/AdvertisementRepository.js
+++ repositories/AdvertisementRepository.js
@@ -1,7 +1,43 @@
 const { default: mongoose } = require("mongoose");
 const Advertisement = require("../entities/Advertisement");
 const AdvertisementPackage = require("../entities/AdvertisementPackage");
+const cron = require("node-cron");
 
+cron.schedule("* * * * *", async () => {
+  const advertisements = await Advertisement.find({
+    expDate: { $lt: Date.now() },
+    isAdvertised: true,
+  });
+  advertisements.forEach(async (ads) => {
+    const extendPackage =
+      ads.advertisementPackages[ads.currentPackageIndex + 1];
+    if (extendPackage) {
+      const newPackage = await AdvertisementPackage.findOne({
+        _id: extendPackage,
+      });
+      const dateUnit = newPackage.dateUnit;
+      const numberOfDateUnit = newPackage.numberOfDateUnit;
+      const newExpDate = ads.expDate;
+      if (dateUnit === "DAY") {
+        newExpDate.setDate(newExpDate.getDate() + numberOfDateUnit);
+      } else if (dateUnit === "MONTH") {
+        newExpDate.setMonth(newExpDate.getMonth() + numberOfDateUnit);
+      } else {
+        newExpDate.setFullYear(newExpDate.getFullYear() + numberOfDateUnit);
+      }
+      const result = await Advertisement.updateOne(
+        { _id: ads._id },
+        {
+          expDate: newExpDate,
+          $inc: { currentPackageIndex: 1 },
+          rank: newPackage.coin,
+        }
+      );
+    } else {
+      await Advertisement.updateOne({ _id: ads._id }, { isAdvertised: false });
+    }
+  });
+});
 class AdvertisementRepository {
   async createAnAdvertisementRepository(userId, videoId, packageId) {
     try {
@@ -23,9 +59,28 @@ class AdvertisementRepository {
       const advertisement = await Advertisement.create({
         userId,
         videoId,
-        advertisementPackage: new mongoose.Types.ObjectId(packageId),
+        totalCoin: advertisementPackage.coin,
+        advertisementPackages: new mongoose.Types.ObjectId(packageId),
         expDate,
+        rank: advertisementPackage.coin,
       });
+      return advertisement;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async extendAdvertisementRepository(adsId, packageId) {
+    try {
+      const adsPackage = await AdvertisementPackage.findById(packageId);
+      const advertisement = await Advertisement.findByIdAndUpdate(
+        adsId,
+        {
+          $push: { advertisementPackages: adsPackage._id },
+          $inc: { totalCoin: adsPackage.coin },
+        },
+        { new: true }
+      );
       return advertisement;
     } catch (error) {
       throw new Error(error.message);
