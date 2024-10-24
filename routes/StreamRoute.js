38c75b1264f84360c2d50ace73a019cc424700c6
Lê Quý Hoàng Tâm <127521514+ellie2222222@users.noreply.github.com>
2024-10-24 16:36:36 +0700

    feat: add fetch stream algorithm

--- routes/StreamRoute.js
+++ routes/StreamRoute.js
@@ -9,6 +9,41 @@ const streamRoutes = express.Router();
 
 streamRoutes.get("/live-input", streamController.listLiveInputsController);
 
+
+/**
+ * @swagger
+ * /api/streams/relevant:
+ *   get:
+ *     summary: Get streams
+ *     tags: [Streams]
+ *     responses:
+ *      200:
+ *       description: Create stream successfully
+ *      400:
+ *       description: Bad request
+ *      500:
+ *       description: Internal server error
+ *
+ */
+streamRoutes.get("/relevant", AuthMiddleware, streamController.getRelevantStreamsController);
+
+/**
+ * @swagger
+ * /api/streams/recommendation:
+ *   get:
+ *     summary: Get streams
+ *     tags: [Streams]
+ *     responses:
+ *      200:
+ *       description: Create stream successfully
+ *      400:
+ *       description: Bad request
+ *      500:
+ *       description: Internal server error
+ *
+ */
+streamRoutes.get("/recommendation", AuthMiddleware, streamController.getRecommendedStreamsController);
+
 // streamRoutes.post("/live-input", AuthMiddleware, streamController.createLiveInputController);
 
 // streamRoutes.put(
