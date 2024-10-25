aa8a66e4c0f0be23c46d40404691e3c28e1b5deb
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-10-25 14:13:02 +0700

    feat: pay with Paypal

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -1,4 +1,12 @@
+require("dotenv").config();
 const paypal = require("paypal-rest-sdk");
+const { convertMoney } = require("../services/PaymentService");
+const {
+  processPaymentQueue,
+  consumePaymentQueue,
+  consumeResponseQueue,
+} = require("../queues/paymentQueues");
+const StatusCodeEnums = require("../enums/StatusCodeEnum");
 paypal.configure({
   mode: "sandbox",
   client_id: process.env.PAYPAL_CLIENT_ID,
@@ -6,8 +14,7 @@ paypal.configure({
 });
 class PaymentController {
   async payWithPayPalController(req, res) {
-    const userId =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzBmM2M5MjFiOWRiZDhkNmVhMTgzZjciLCJpcCI6Ijo6MSIsImlhdCI6MTcyOTc0MDE2MSwiZXhwIjoxNzI5NzYxNzYxfQ.9US6JVWg9HMAFzGC6cM6Aia_XNIP2zFBlrNmMZtTZPA";
+    const userId = req.userId;
     req.session.userId = userId;
 
     const create_payment_json = {
@@ -16,8 +23,8 @@ class PaymentController {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "http://localhost:4000/api/payment/paypal/success",
-        cancel_url: "http://localhost:4000/api/payment/paypal/cancel",
+        return_url: process.env.PAYPAL_SUCCESS_URL,
+        cancel_url: process.env.PAYPAL_CANCEL_URL,
       },
       transactions: [
         {
@@ -59,30 +66,41 @@ class PaymentController {
     const userId = req.session.userId;
     const execute_payment_json = {
       payer_id: payerId,
-      //   transactions: [
-      //     {
-      //       amount: {
-      //         currency: "USD",
-      //         total: "25.00",
-      //       },
-      //     },
-      //   ],
     };
 
     paypal.payment.execute(
       paymentId,
       execute_payment_json,
-      function (error, payment) {
+      async function (error, payment) {
         if (error) {
           console.log(error.response);
           throw error;
         } else {
-          console.log(payment);
-          console.log(payment.transactions[0].amount.total);
-          res.send("Success");
+          const amountInVnd = await convertMoney(
+            Number(payment.transactions[0].amount.total),
+            "USD",
+            "VND"
+          );
+          const paymentParams = {
+            id: payment.id,
+            paymentMethod: "PAYPAL",
+            paymentPort: "PAYPAL",
+          };
+          await processPaymentQueue(userId, amountInVnd, paymentParams);
+          await consumePaymentQueue();
+          await consumeResponseQueue();
+          res
+            .status(StatusCodeEnums.OK_200)
+            .json({ message: "Payment successfully." });
         }
       }
     );
+  }
+
+  async cancelPayPalController(req, res) {
+    res.status(StatusCodeEnums.BadRequest_400).json({
+      message: "Payment failed.",
+    });
   }
 }
 
