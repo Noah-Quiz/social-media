a2004ec8b1b07953a2ad60ba6d0a5ec69b731a45
QuangNV23062004 <quangnv23062004@gmail.com>
2024-11-08 14:29:07 +0700

    adjust id in history, adjust config vnpay

--- services/HistoryService.js
+++ services/HistoryService.js
@@ -1,56 +1,77 @@
+const StatusCodeEnums = require("../enums/StatusCodeEnum");
+const CoreException = require("../exceptions/CoreException");
 const DatabaseTransaction = require("../repositories/DatabaseTransaction");
 
 const createHistoryRecordService = async (data) => {
-    try {
-        const connection = new DatabaseTransaction();
-
-        const historyRecord = await connection.historyRepository.createHistoryRecordRepository(data);
-
-        return historyRecord;
-    } catch (error) {
-        throw error;
+  try {
+    const connection = new DatabaseTransaction();
+    const checkUser = await connection.userRepository.getAnUserByIdRepository(
+      data.userId
+    );
+    if (!checkUser || checkUser === false) {
+      throw new CoreException(StatusCodeEnums.BadRequest_400, "User not found");
     }
-}
+    const checkVideo = await connection.videoRepository.getVideoByIdRepository(
+      data.videoId
+    );
+    if (!checkVideo) {
+      throw new CoreException(
+        StatusCodeEnums.BadRequest_400,
+        "Video not found"
+      );
+    }
+    const historyRecord =
+      await connection.historyRepository.createHistoryRecordRepository(data);
+
+    return historyRecord;
+  } catch (error) {
+    throw error;
+  }
+};
 
 const clearAllHistoryRecordsService = async (userId) => {
-    try {
-        const connection = new DatabaseTransaction();
+  try {
+    const connection = new DatabaseTransaction();
 
-        await connection.historyRepository.clearAllHistoryRecordsRepository(userId);
+    await connection.historyRepository.clearAllHistoryRecordsRepository(userId);
 
-        return;
-    } catch (error) {
-        throw error;
-    }
-}
+    return;
+  } catch (error) {
+    throw error;
+  }
+};
 
 const deleteHistoryRecordService = async (historyId) => {
-    try {
-        const connection = new DatabaseTransaction();
+  try {
+    const connection = new DatabaseTransaction();
 
-        await connection.historyRepository.deleteHistoryRecordRepository(historyId);
+    await connection.historyRepository.deleteHistoryRecordRepository(historyId);
 
-        return;
-    } catch (error) {
-        throw error;
-    }
-}
+    return;
+  } catch (error) {
+    throw error;
+  }
+};
 
 const getAllHistoryRecordsService = async (userId, query) => {
-    try {
-        const connection = new DatabaseTransaction();
+  try {
+    const connection = new DatabaseTransaction();
 
-        const data = await connection.historyRepository.getAllHistoryRecordsRepository(userId, query);
+    const data =
+      await connection.historyRepository.getAllHistoryRecordsRepository(
+        userId,
+        query
+      );
 
-        return data;
-    } catch (error) {
-        throw error;
-    }
-}
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
-    createHistoryRecordService,
-    clearAllHistoryRecordsService,
-    getAllHistoryRecordsService,
-    deleteHistoryRecordService,
-}
+  createHistoryRecordService,
+  clearAllHistoryRecordsService,
+  getAllHistoryRecordsService,
+  deleteHistoryRecordService,
+};
