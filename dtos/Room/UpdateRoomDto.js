0c83ee33ac17501a1ebd80f1d4cac1fb0077fb52
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-11-26 16:24:40 +0700

    refactor: clean up code formatting and improve validation function in UpdateRoomDto and validator utility

--- dtos/Room/UpdateRoomDto.js
+++ dtos/Room/UpdateRoomDto.js
@@ -3,26 +3,26 @@ const CoreException = require("../../exceptions/CoreException");
 const { validMongooseObjectId } = require("../../utils/validator");
 
 class UpdateRoomDto {
-    constructor(roomId) {
-        this.roomId = roomId;
-    }
+  constructor(roomId) {
+    this.roomId = roomId;
+  }
 
-    async validate() {
-        if (!this.roomId) {
-            throw new CoreException(
-                StatusCodeEnums.BadRequest_400,
-                "Room ID is required"
-            );
-        }
-        try {
-            validMongooseObjectId(this.roomId);
-        } catch (error) {
-            throw new CoreException(
-                StatusCodeEnums.BadRequest_400,
-                "Invalid Room ID"
-            );
-        }
+  async validate() {
+    if (!this.roomId) {
+      throw new CoreException(
+        StatusCodeEnums.BadRequest_400,
+        "Room ID is required"
+      );
     }
+    try {
+      validMongooseObjectId(this.roomId);
+    } catch (error) {
+      throw new CoreException(
+        StatusCodeEnums.BadRequest_400,
+        "Invalid Room ID"
+      );
+    }
+  }
 }
 
 module.exports = UpdateRoomDto;
