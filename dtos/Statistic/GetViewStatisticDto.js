907a387aef3af2f59689c0e92f6f4d338f9bdc42
QuangNV23062004 <nguyenquang23062004@gmail.com>
2024-11-28 11:11:56 +0700

    adjust validation, requester, role validation, add swagger to video view statistic

--- dtos/Statistic/GetViewStatisticDto.js
+++ dtos/Statistic/GetViewStatisticDto.js
@@ -1,6 +1,7 @@
 const StatusCodeEnums = require("../../enums/StatusCodeEnum");
 const CoreException = require("../../exceptions/CoreException");
 const { validMongooseObjectId } = require("../../utils/validator");
+const dayjs = require("dayjs");
 
 class GetViewStatisticDto {
   constructor(ownerId, TimeUnit, value) {
@@ -28,17 +29,24 @@ class GetViewStatisticDto {
         "Invalid TimeUnit"
       );
     }
-
+    const currentDate = dayjs();
     if (this.TimeUnit === "MONTH" && this.value !== undefined) {
       if (
         isNaN(this.value) || // Must be a number
-        !Number.isInteger(this.value) || // Must be an integer
+        !Number.isInteger(Number(this.value)) || // Must be an integer
         this.value < 1 || // Must be at least 1
         this.value > 12 // Cannot exceed 12
       ) {
         throw new CoreException(
           StatusCodeEnums.BadRequest_400,
           "Invalid value in month case: value must be an integer between 1 and 12"
+        );
+      }
+      const currentMonth = currentDate.month() + 1; // dayjs months are 0-indexed
+      if (this.value > currentMonth) {
+        throw new CoreException(
+          StatusCodeEnums.BadRequest_400,
+          "Invalid value in month case: cannot specify a future month"
         );
       }
     }
@@ -57,6 +65,13 @@ class GetViewStatisticDto {
         throw new CoreException(
           StatusCodeEnums.BadRequest_400,
           "Invalid value in year case: year must be a positive integer starting from 2024"
+        );
+      }
+      const currentYear = currentDate.year();
+      if (this.value > currentYear) {
+        throw new CoreException(
+          StatusCodeEnums.BadRequest_400,
+          "Invalid value in year case: cannot specify a future year"
         );
       }
     }
